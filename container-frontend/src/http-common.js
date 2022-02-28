023ae485fd7a7c22271e442c71a0c00bda907c0a
salman9000 <salmanhanif133@gmail.com>
2022-02-28 17:12:08 +0500

    done

--- container-frontend/src/http-common.js
+++ container-frontend/src/http-common.js
@@ -10,7 +10,7 @@ const http = axios.create({
 });
 
 const localHttp = axios.create({
-  baseURL: "http://localhost:3001/api/",
+  baseURL: "https://tranquil-dawn-42923.herokuapp.com/api/",
   headers: {
     "Content-type": "application/json",
   }
